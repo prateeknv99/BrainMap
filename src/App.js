29307353b9c7013d3d308a3b79461bcb2e1a804c
Prateek <prateeknv99@gmail.com>
2019-11-03 06:44:47 +0530

    UI about to complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, {Component} from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {Switch, Route, Redirect} from "react-router-dom";
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Main from './components/Main';
 import Nav from './components/Nav';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Nav />
-      </header>
-      <Main />
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <header className="App-header">
+          <Nav />
+        </header>
+        {/* <Main /> */}
+        <Switch>
+          <Route exact path="/" component={Main} />
+          {/* <Route path="/product/:itemType/:id" render={(props)=> <Product {...props} data={this.state.data} sendItemToCart = {(newItem)=>{this.newCartItem = newItem}} />} /> */}
+          <Redirect to="/" />
+      </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Select from 'react-select';
 
+import Chart from 'react-google-charts';
+
 const options = [
-  { value: '0', label: '0' },
-  { value: '1', label: '1' },
-  { value: '2', label: '2' },
-  { value: '3', label: '3' },
-  { value: '4', label: '4' },
-  { value: '5', label: '5' },
-  { value: '6', label: '6' },
-  { value: '7', label: '7' },
-  { value: '8', label: '8' },
-  { value: '9', label: '9' },
+  { value: 0, label: '0 - Lights Off' },
+  { value: 1, label: '1 - Lights On' },
+  { value: 2, label: '2 - Fan Off' },
+  { value: 3, label: '3 - Fan On' },
+  { value: 4, label: '4 - Hunger' },
+  { value: 5, label: '5 - Alert' },
+  { value: 6, label: '6 - ...' },
+  { value: 7, label: '7 - ...' },
+  { value: 8, label: '8 - ...' },
+  { value: 9, label: '9 - ...' },
 ];
 
 class Main extends React.Component {
@@ -24,21 +26,30 @@ class Main extends React.Component {
 
     state = {
       selectedOption: null,
+      selectedOptionVal: 0,
     }
 
     handleChange = selectedOption => {
-      this.setState({ selectedOption });
-      console.log(`Option selected:`, selectedOption);
+      this.setState({ selectedOption: selectedOption });
+      if((selectedOption.value === 1) || (selectedOption.value === 0)) {
+        this.setState({ selectedOptionVal: selectedOption.value });
+      }
     };
 
     render() {
-      const { selectedOption } = this.state;
+      const { selectedOptionVal, selectedOption } = this.state;
+      if(selectedOptionVal === 0) {
+        var imgComponent = <img className="Main-img" src="/images/light_off.jpg" alt=""/>;
+      }else{
+        imgComponent = <img className="Main-img" src="/images/light_on.jpg" alt=""/>;
+      }
       return(
         <div className="Main">
           <div className="Main-overlay">
             <div className="Main-content">
               <div>
-                <img className="Main-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSqfCpzjglOZEfyx_4p6NFUHh1qQE50yIdUX0uYmyiNQbx78MNx" alt=""/>
+                {/* <img className="Main-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSqfCpzjglOZEfyx_4p6NFUHh1qQE50yIdUX0uYmyiNQbx78MNx" alt=""/> */}
+                {imgComponent}
               </div>
               <div className="main-inside-box">
                 Select Random Class from dataset 
@@ -52,7 +63,51 @@ class Main extends React.Component {
                 <br/>
                 <button className="btn btn-primary" type="submit">Proceed</button>
               </div>
+              
             </div>
+            <br/>
+            <div className="Main-content" style={{marginBottom: '30px'}}>
+              {/* <div>
+                
+              </div> */}
+              <div className="main-inside-box" style={{width:'80%', paddingTop: '50px'}}>
+                Probability Distribution Bar Graph of predicted digits
+                <br/>
+                <br/> 
+
+                <Chart
+                  width={'800px'}
+                  height={'300px'}
+                  chartType="Bar"
+                  loader={<div>Loading Probability Distribution Chart</div>}
+                  data={[
+                    ['Digits', 'Probability'],
+                    ['0', 0.26],
+                    ['1', 0.81],
+                    ['2', 0.19],
+                    ['3', 0.18],
+                    ['4', 0.26],
+                    ['5', 0.22],
+                    ['6', 0.19],
+                    ['7', 0.05],
+                    ['8', 0.04],
+                    ['9', 0.29],
+                  ]}
+                  options={{
+                    // Material design options
+                    chart: {
+                      title: ' ',
+                      chartArea: {'width': '100%', 'height': '80%'},
+                    },
+                  }}
+                  // For tests
+                  rootProps={{ 'data-testid': '2' }}
+                />
+
+              </div>
+              
+            </div>
+
           </div>
 
         </div>
